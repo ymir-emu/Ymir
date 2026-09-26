76e09361a8b253ad558aad7ccafffdfd02a1f45d
StrikerX3 <StrikerX3@users.noreply.github.com>
2026-09-26 13:44:53 -0300

    chore: Move headless debugger out of the main repository
    See https://github.com/ymir-emu/ymir-headless

--- apps/ymir-headless/src/toml_implementation.cpp
+++
@@ -1,2 +0,0 @@
-#define TOML_IMPLEMENTATION
-#include <toml++/toml.hpp>
--- tests/ymir-headless-tests/src/smoke_tests.cpp
+++
@@ -1,5 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("ymir-headless smoke", "[smoke]") {
-    REQUIRE(true);
-}
